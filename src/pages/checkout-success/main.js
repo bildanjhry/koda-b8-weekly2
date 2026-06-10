e74aa3c308b026a759d923580080cd6de3476767
bildan <bildanjauhary@gmail.com>
2026-06-11 03:37:55 +0700

    feat: add logic to clear cart as finnishing actions

--- src/pages/checkout-success/main.js
+++ src/pages/checkout-success/main.js
@@ -3,7 +3,10 @@ import moneyFormat from "../../js/utils/money-format.js"
 $(document).ready(function(){
     const userData = JSON.parse(window.localStorage.getItem("user"))
     const checkout = userData.checkout
-    
+    window.localStorage.setItem("user", JSON.stringify({
+        ...userData,
+        cart:[]
+    }))
     $(".transactions-total > h4").text(moneyFormat(userData.checkoutTotal)[0])
     $(".delivery-method > p:first-child").text(userData.deliveryMethod[0])
     $(".delivery-method > p:last-child > span").text(userData.deliveryMethod[1])
